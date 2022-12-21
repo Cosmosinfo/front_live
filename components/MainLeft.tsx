edc07193237ff1d405c93ec89b6a0167f6d3eb49
tkddus614 <tkddus614@naver.com>
2022-12-21 18:28:41 +0900

    page add

--- components/MainLeft.tsx
+++ components/MainLeft.tsx
@@ -14,13 +14,12 @@ const MainLeft = () => {
       imgUrl: "/images/house.svg",
       hoverImgUrl: "/images/house_white.svg",
       nowImgUrl: "/images/house_white.svg",
-      pathname: "/home",
+      pathname: "/",
     },
     {
       id: 2,
       name: "스테이지",
       imgUrl: "/images/stage.svg",
-      hoverImgUrl: "/images/stage_white.svg",
       nowImgUrl: "/images/stage_white.svg",
       pathname: "/stage",
     },
@@ -28,7 +27,6 @@ const MainLeft = () => {
       id: 3,
       name: "아티스트",
       imgUrl: "/images/artist.svg",
-      hoverImgUrl: "/images/artist_white.svg",
       nowImgUrl: "/images/artist_white.svg",
       pathname: "/artist",
     },
@@ -36,7 +34,6 @@ const MainLeft = () => {
       id: 4,
       name: "스토어",
       imgUrl: "/images/shop.svg",
-      hoverImgUrl: "/images/shop_white.svg",
       nowImgUrl: "/images/shop_white.svg",
       pathname: "/store",
     },
@@ -44,7 +41,6 @@ const MainLeft = () => {
       id: 5,
       name: "설정",
       imgUrl: "/images/setting.svg",
-      hoverImgUrl: "/images/setting_white.svg",
       nowImgUrl: "/images/setting_white.svg",
       pathname: "/setting",
     },
@@ -52,9 +48,8 @@ const MainLeft = () => {
       id: 6,
       name: "도움말",
       imgUrl: "/images/flag.svg",
-      hoverImgUrl: "/images/flag_white.svg",
       nowImgUrl: "/images/flag_white.svg",
-      pathname: "/support",
+      pathname: "/help",
     },
   ];
 
@@ -62,13 +57,7 @@ const MainLeft = () => {
     <Nav>
       {menus.map((menu, index) => (
         <Link href={menu.pathname} key={index} legacyBehavior passHref>
-          <NavInfo
-            key={index}
-            imgUrl={menu.imgUrl}
-            hoverImgUrl={menu.hoverImgUrl}
-            isHere={pathname === menu.pathname}
-            nowImgUrl={menu.nowImgUrl}
-          >
+          <NavInfo key={index} imgUrl={menu.imgUrl} isHere={pathname === menu.pathname} nowImgUrl={menu.nowImgUrl}>
             {menu.name}
           </NavInfo>
         </Link>
@@ -87,19 +76,26 @@ const Nav = styled.nav`
   flex-direction: column;
 `;
 
-const NavInfo = styled.a<{ imgUrl: string; hoverImgUrl: string; nowImgUrl: string; isHere: boolean }>`
+const NavInfo = styled.a<{ imgUrl: string; nowImgUrl: string; isHere: boolean }>`
   display: inline-block;
+  min-height: 40px;
   margin-bottom: 40px;
   background-position: 0 50%;
-  background-size: 30px;
+  background-size: contain;
   padding-left: 40px;
   ${(props) =>
     props.isHere
-      ? `color: #ffffff; background:url(${props.nowImgUrl}) no-repeat`
-      : `color: #8B8B8B; background: url(${props.imgUrl}) no-repeat;`}
+      ? `
+      color: #ffffff;
+      background: url(${props.nowImgUrl}) no-repeat left 50%;
+  `
+      : `
+      color: #8B8B8B;
+      background: url(${props.imgUrl}) no-repeat;
+  `}
   :hover {
     color: #ffffff;
-    ${(props) => `background: url(${props.hoverImgUrl}) no-repeat`}
+    ${(props) => `background: url(${props.nowImgUrl}) no-repeat`}
   }
 `;
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,10 @@
 import Main from "components/Main";
-import MainLeft from "components/MainLeft";
-import MainTop from "components/MainTop";
 import React from "react";
-import styled from "styled-components";
 
 export default function Home() {
   return (
     <>
-      <MainTop />
-      <Wrap>
-        <MainLeft />
-        <Main />
-      </Wrap>
+      <Main />
     </>
   );
 }
-
-const Wrap = styled.div`
-  display: flex;
-`;
