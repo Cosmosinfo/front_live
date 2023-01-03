79bbe2956e7f73440cf6e389729f981dad6b0192
tkddus614 <tkddus614@naver.com>
2023-01-03 16:53:34 +0900

    chore: login form publishing

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,36 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState, ChangeEvent } from "react";
+import styled, { css } from "styled-components";
 
 const Login = () => {
+  const [passwordType, setPasswordType] = useState({
+    type: "password",
+    visible: false,
+  });
+  const [values, setValues] = useState({
+    email: "",
+    emailSite: "",
+    password: "",
+  });
+
+  const { email, emailSite, password } = values;
+
+  const onChangeValues = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handlePasswordType = () => {
+    setPasswordType(() => {
+      if (!passwordType.visible) {
+        return { type: "text", visible: true };
+      }
+      return { type: "password", visible: false };
+    });
+  };
+
   return (
     <Container>
       <Wrap>
@@ -11,20 +40,189 @@ const Login = () => {
               로그인<p>풀다이브에 오신것을 환영합니다.</p>
             </Title>
             <SocialLogin>
-              <SosicalLoginButton>구글 계정으로 로그인</SosicalLoginButton>
-              <SosicalLoginButton>구글 계정으로 로그인</SosicalLoginButton>
+              <div>
+                <SosicalLoginButton social="google">구글 계정으로 로그인</SosicalLoginButton>
+                <SosicalLoginButton social="line">라인 계정으로 로그인</SosicalLoginButton>
+              </div>
+              <div>
+                <SosicalLoginButton social="apple">애플 계정으로 로그인</SosicalLoginButton>
+                <SosicalLoginButton social="kakao">카카오 계정으로 로그인</SosicalLoginButton>
+              </div>
             </SocialLogin>
           </Top>
+          <Or>또는</Or>
+          <LoginForm>
+            <p>이메일</p>
+            <EmailForm>
+              <Email name="email" type="text" placeholder="이메일 선택" autoComplete="off" value={email} onChange={onChangeValues} />
+              <span>@</span>
+              <EmailSite name="emailSite" placeholder="이메일 선택" autoComplete="off" value={emailSite} onChange={onChangeValues} />
+            </EmailForm>
+            <p>비밀번호</p>
+            <PasswordForm>
+              <Password name="password" type={passwordType.type} autoComplete="off" value={password} onChange={onChangeValues} />
+              <VisibleIcon onClick={handlePasswordType} type={passwordType.type} />
+            </PasswordForm>
+            <LoginButton>접속하기</LoginButton>
+            <Bottom>
+              <p className="mssing_user">아이디 / 비밀번호 찾기</p>
+              <p>계정이 없으신가요?</p>
+            </Bottom>
+          </LoginForm>
         </Form>
       </Wrap>
     </Container>
   );
 };
 
-const SosicalLoginButton = styled.button`
-  padding: 20px 40px 20px 70px;
+const Bottom = styled.div`
+  margin-top: 12px;
+  display: flex;
+  justify-content: space-between;
+  font-size: 1rem;
+  color: #8692ff;
+  > p {
+    &.mssing_user {
+      color: #b7b7b7;
+    }
+  }
+`;
+
+const PasswordForm = styled.div`
+  position: relative;
+`;
+
+const LoginButton = styled.button`
+  margin-top: 50px;
+  width: 100%;
+  height: 60px;
+  border: none;
+  border-radius: 200px;
+  background-color: #273dff;
+`;
+
+const VisibleIcon = styled.div<{ type: string }>`
+  position: absolute;
+  top: 40%;
+  right: 20px;
+  width: 24px;
+  height: 22px;
+  cursor: pointer;
+  background: url("/images/login/password_visible.svg") no-repeat;
+  ${({ type }) => type === "text" && ` background: url("/images/login/password_visible_white.svg") no-repeat;`}
+`;
+
+const Password = styled.input`
+  width: 100%;
+  margin-top: 10px;
+  color: inherit;
+  border: none;
+  border-radius: 100px;
+  padding: 20px 40px 20px 60px;
+  background: url("/images/login/password.svg") no-repeat 25px 50% #28282f;
+  :focus {
+    border: 2px solid #273dff;
+    background: url("/images/login/password_white.svg") no-repeat 25px 50% #28282f;
+  }
+`;
+
+const EmailSite = styled.input`
+  margin-top: 10px;
+  max-width: 240px;
+  height: 60px;
+  border: none;
+  border-radius: 100px;
+  padding: 20px 40px;
+  background-color: #28282f;
+  color: inherit;
+`;
+
+const Email = styled.input`
+  margin-top: 10px;
+  max-width: 240px;
+  color: inherit;
+  height: 60px;
+  border: none;
+  border-radius: 100px;
+  padding: 20px 40px 20px 60px;
+  background: url("/images/login/email.svg") no-repeat 25px 50% #28282f;
+  :focus {
+    border: 2px solid #273dff;
+    background: url("/images/login/email_white.svg") no-repeat 25px 50% #28282f;
+  }
+`;
+
+const EmailForm = styled.div`
+  display: flex;
+  justify-content: space-between;
+  > span {
+    color: #aeaeae;
+    margin: auto 0;
+  }
+`;
+
+const LoginForm = styled.form`
+  > p {
+    margin-top: 35px;
+  }
+`;
+
+const Or = styled.div`
+  display: flex;
+  flex-basis: 100%;
+  align-items: center;
+  font-size: 1.25rem;
+  margin-top: 35px;
+  ::before {
+    content: "";
+    flex-grow: 1;
+    background: rgba(99, 99, 99, 1);
+    height: 1px;
+    font-size: 0px;
+    line-height: 0px;
+    margin-right: 16px;
+  }
+  ::after {
+    content: "";
+    flex-grow: 1;
+    background: rgba(99, 99, 99, 1);
+    height: 1px;
+    font-size: 0px;
+    line-height: 0px;
+    margin-left: 16px;
+  }
+`;
+
+const SosicalLoginButton = styled.button<{ social: string }>`
+  min-width: 248px;
+  height: 60px;
+  padding: 20px 40px 20px 60px;
   margin-top: 36px;
-  background: url("/images/login/google.svg") no-repeat 35px 50% #202024;
+  ${({ social }) => {
+    switch (social) {
+      case "google":
+        return css`
+          background: url("/images/login/google.svg") no-repeat 35px 50% #28282f;
+          margin-right: 24px;
+        `;
+      case "line":
+        return css`
+          background: url("/images/login/line.svg") no-repeat 35px 50% #28282f;
+        `;
+      case "apple":
+        return css`
+          background: url("/images/login/apple.svg") no-repeat 35px 50% #28282f;
+          margin-right: 24px;
+          margin-top: 12px;
+        `;
+      case "kakao":
+        return css`
+          background: url("/images/login/kakao.svg") no-repeat 25px 50% #28282f;
+          padding-left: 50px;
+          margin-top: 12px;
+        `;
+    }
+  }}
   border-radius: 100px;
   font-size: 1rem;
 `;
@@ -52,13 +250,13 @@ const Form = styled.div`
 const Wrap = styled.div`
   min-width: 584px;
   height: 698px;
-  background: #181820;
-  margin: 5% auto;
+  margin: 36px auto;
   border-radius: 24px; ;
 `;
 
 const Container = styled.main`
   width: calc(100vw - 250px);
+  height: 100vh;
   background: #1e1e1e;
   color: #ffffff;
   display: flex;
