e827f0592f5b8f975098147d85102ca189d96e0f
tkddus614 <tkddus614@naver.com>
2023-03-06 14:31:12 +0900

    paypal

--- components/PaymentNext.tsx
+++ components/PaymentNext.tsx
@@ -1,12 +1,64 @@
 import React from "react";
 import styled from "styled-components";
+import { PayPalScriptProvider, PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import { PayPalScriptOptions } from "@paypal/paypal-js/types/script-options";
+import { PayPalButtonsComponentProps } from "@paypal/paypal-js/types/components/buttons";
+
+const paypalScriptOptions: PayPalScriptOptions = {
+  "client-id": `${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}`,
+  "currency": "USD",
+};
 
 const data = {
   comet: 40,
   ticket: 30,
 };
 
+function Button() {
+  /**
+   * usePayPalScriptReducer use within PayPalScriptProvider
+   * isPending: not finished loading(default state)
+   * isResolved: successfully loaded
+   * isRejected: failed to load
+   */
+  const [{ isPending }] = usePayPalScriptReducer();
+  const paypalbuttonTransactionProps: PayPalButtonsComponentProps = {
+    style: { layout: "vertical" },
+    createOrder(data, actions) {
+      return actions.order.create({
+        purchase_units: [
+          {
+            amount: {
+              value: "0.01",
+            },
+          },
+        ],
+      });
+    },
+    onApprove(data, actions) {
+      /**
+       * data: {
+       *   orderID: string;
+       *   payerID: string;
+       *   paymentID: string | null;
+       *   billingToken: string | null;
+       *   facilitatorAccesstoken: string;
+       * }
+       */
+      return actions.order.capture({}).then((details: any) => {
+        alert("Transaction completed by" + (details?.payer.name.given_name ?? "No details"));
 
+        alert("Data details: " + JSON.stringify(data, null, 2));
+      });
+    },
+  };
+  return (
+    <>
+      {isPending ? <h2>Load Smart Payment Button...</h2> : null}
+      <PayPalButtons {...paypalbuttonTransactionProps} />
+    </>
+  );
+}
 
 const PaymentNext = () => {
   return (
@@ -30,10 +82,17 @@ const PaymentNext = () => {
           <div>{data && Number(data.comet) + Number(data.ticket)}</div>
         </Bottom>
       </CometWrap>
-      <div>카드 정보 입력</div>
+      <Card>카드 정보 입력</Card>
+      <PayPalScriptProvider options={paypalScriptOptions}>
+        <Button />
+      </PayPalScriptProvider>
     </>
   );
 };
+
+const Card = styled.div`
+  margin-bottom: 20px;
+`;
 
 const Payment = styled.div`
   display: flex;
